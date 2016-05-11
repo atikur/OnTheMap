14d55a39e643dfe4eae5cf661bc23d08d3b4566b
Atikur Rahman <atik.bd08@gmail.com>
2016-05-11 08:48:40 +0600

    rename ViewController to LoginViewController

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Atikur Rahman on 5/11/16.
+//  Copyright © 2016 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
+
