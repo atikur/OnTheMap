850fe552194fbf65b5a4410ed0b469deeb7f763b
Atikur Rahman <atik.bd08@gmail.com>
2016-05-12 16:06:05 +0600

    add StudentMapViewController

---
+++ OnTheMap/StudentMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StudentMapViewController.swift
+//  OnTheMap
+//
+//  Created by Atikur Rahman on 5/12/16.
+//  Copyright © 2016 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class StudentMapViewController: UIViewController {
+    
+}
