fdcfe09af514f0b92e4579e30e4e3c3df83f175b
Atikur Rahman <atik.bd08@gmail.com>
2016-05-13 16:52:54 +0600

    add PostInfoViewController

---
+++ OnTheMap/PostInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostInfoViewController.swift
+//  OnTheMap
+//
+//  Created by Atikur Rahman on 5/13/16.
+//  Copyright © 2016 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class PostInfoViewController: UIViewController {
+    
+    @IBAction func cancelButtonPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
