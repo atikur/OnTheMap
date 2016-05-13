2d3284893460e1951305ee14006fb9c25840fe8a
Atikur Rahman <atik.bd08@gmail.com>
2016-05-13 17:26:34 +0600

    add UI controls to PostInfoViewController

--- OnTheMap/PostInfoViewController.swift
+++ OnTheMap/PostInfoViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
-class PostInfoViewController: UIViewController {
+class PostInfoViewController: UIViewController, UITextViewDelegate {
+    
+    let textViewPlaceholderText = "Enter Your Location Here"
+    
+    @IBOutlet weak var locationTextView: UITextView!
+    @IBOutlet weak var findOnMapButton: UIButton!
     
     @IBAction func cancelButtonPressed(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func findOnMapButtonPressed(sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationTextView.delegate = self
+        locationTextView.text = textViewPlaceholderText
+        findOnMapButton.layer.cornerRadius = 10
+    }
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.text == textViewPlaceholderText {
+            textView.text = ""
+        }
     }
 }
